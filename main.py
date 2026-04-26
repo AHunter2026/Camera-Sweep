d6641ef57d9abfa7d7ead25a7994d4f519a66831
Ashley <ahun429@wgu.edu>
2026-04-25 19:58:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from machine import Pin, PWM
+from time import sleep, sleep_ms
+import sys
+
+servo = PWM(Pin(0))
+servo.freq(50)
+
+
+def set_angle(angle):
+    min_duty = 1638
+    max_duty = 8192
+    duty = int(min_duty + (max_duty - min_duty) * angle / 180)
+    servo.duty_u16(duty)
+
+wait_seconds = 30
+
+print()
+print(">>> Sweep Program <<<")
+print()
+print("**Starting NOW**")
+print()
+
+for cycle in range(1,3):
+    print(f"*** Cycle {cycle} ***")
+
+    # Sweep 20 times
+    for i in range(1, 21):
+        print(f" Sweep {i} of 20 ")
+        for pos in range(0, 181, 5):
+            set_angle(pos)
+            sleep_ms(10)
+        for pos in range(180, -1, -5):
+            set_angle(pos)
+            sleep_ms(10)
+
+    # Countdown
+    if cycle != 2:
+        print(f"{i} sweeps done!")
+        for remaining in range(wait_seconds, 0, -1):
+            sys.stdout.write(f"\r Next cycle in {remaining} seconds... ")
+            sleep(1)
+        print()
+
+print("*****")
+print("All done!")
+print("Thank you for using!")
+print("*****")
