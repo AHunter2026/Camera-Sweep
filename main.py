3eae89c6dd25f0c070db4f6f29a6a0f5fdb25482
Ashley <ahun429@wgu.edu>
2026-04-25 22:01:06 -0500

    Took out ending message and also the total amount of sweeps completed after the first cycle. Also made the loop continuous

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ def set_angle(angle):
     duty = int(min_duty + (max_duty - min_duty) * angle / 180)
     servo.duty_u16(duty)
 
+
 wait_seconds = 30
+cycle = 1
 
 print()
 print(">>> Sweep Program <<<")
 print()
-print("**Starting NOW**")
-print()
 
-for cycle in range(1,3):
-    print(f"*** Cycle {cycle} ***")
+while True:
+    print(f"--- Cycle {cycle} ---")
 
     # Sweep 20 times
     for i in range(1, 21):
-        print(f" Sweep {i} of 20 ")
+        print(f"  Sweep {i} of 20")
         for pos in range(0, 181, 5):
             set_angle(pos)
             sleep_ms(10)
@@ -34,8 +34,9 @@ for cycle in range(1,3):
             sleep_ms(10)
 
     # Countdown
-    if cycle != 2:
-        for remaining in range(wait_seconds, 0, -1):
-            sys.stdout.write(f"\r Next cycle in {remaining} seconds... ")
-            sleep(1)
-        print()
+    for remaining in range(wait_seconds, 0, -1):
+        sys.stdout.write(f"\r  Next cycle in {remaining} seconds...  ")
+        sleep(1)
+    print()
+
+    cycle += 1
